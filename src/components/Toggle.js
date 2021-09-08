bf223aeb8d31d9976751bc37075e899b814b59f8
nicmlu <NicoleMLucien@gmail.com>
2021-09-07 23:30:56 -0400

    Done.

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [togg, setTogg] = useState(false);
+
+  function flip() {
+    setTogg(togg => !togg);
+  }
+
+  const color = togg ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={flip}>
+      {togg ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
